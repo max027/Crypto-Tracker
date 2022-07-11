7c1f332d0ae33ab5358c2a33bcd4e31b65e32254
max <max@DESKTOP-A806Q8K.localdomain>
2022-07-11 15:09:20 +0530

    updated login form view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,27 @@
 <script setup>
 import Nav from '@/components/Nav.vue'
+import { onMounted } from 'vue'
+import {useAuthStore} from '@/stores/Auth.js'
+import axios from 'axios'
+
+const store=useAuthStore()
+
+onMounted(() => {
+store.initializeStore()
+const token=store.token
+
+if(token){
+
+    axios.defaults.headers.common['Authorization']="Token "+token
+
+}else{
+
+    axios.defaults.headers.common['Authorization']=''
+
+}
+})
+
+
 </script>
 
 <template>
--- frontend/src/stores/Auth.js
+++ frontend/src/stores/Auth.js
@@ -6,5 +6,24 @@ export const useAuthStore=defineStore({
         isAuthenticated:false
         token:''
     },
-
+   actions:{
+        initializeStore(){
+            if(localStorage.getItem('token')){
+                this.token=localStorage.getItem('token')
+                this.isAuthenticated=true
+            }else{
+                this.token='' 
+                this.isAuthenticated=false
+            }
+        },
+        setToken(token){
+            this.token=token
+            this.isAuthenticated=true
+        } ,
+        removeToken(){
+            this.token=''
+            this.isAuthenticated=false
+        }
+        
+     } 
 })
