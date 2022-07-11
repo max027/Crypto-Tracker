2be594be611c522dc4a4eebceb2f0c862607973a
max <max@DESKTOP-A806Q8K.localdomain>
2022-07-11 13:20:39 +0530

    add authNav

---
+++ frontend/src/components/AuthNav.vue
@@ -0,0 +1,16 @@
+<template>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <router-link to="/" class="navbar-item">Crypto Tracker</router-link>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+            <button class="button is-danger">Log out</button>
+        </div>
+      </div>
+    </div>
+</nav>
+</template>
+
