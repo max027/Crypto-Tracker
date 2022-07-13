1fdce0f08045a3afc8661db58183b856080b686d
max <max@gmail.com>
2022-07-13 13:52:44 +0530

    updated market page

---
+++ frontend/src/components/MarketTop.vue
@@ -0,0 +1,132 @@
+<script setup>
+import {ref} from 'vue'
+
+let large_volume=ref([])
+let top=ref([])
+let Highlight=ref([])
+let price_usd
+let top_gainer
+let temp=0
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); 
+}
+
+  fetch('https://data.messari.io/api/v1/assets?fields=id,slug,symbol,metrics/market_data/price_usd')
+ .then(res=>res.json())
+  .then(data=>{
+    price_usd=data.data
+    for(let i=0;i<3;i++){
+      temp=price_usd[i].metrics.market_data.price_usd
+      large_volume.value.push(temp)
+    }
+
+    for (let i = 0; i < 3; i++) {
+      let temp=getRandomInt(4,19)
+      let price=price_usd[temp].metrics.market_data.price_usd
+      let temp_name=price_usd[temp].slug
+      top_gainer={id:i,name:temp_name,value:price}
+      top.value.push(top_gainer)
+    }
+
+    for (let i = 0; i < 3; i++) {
+      let temp=getRandomInt(4,19)
+      let price=price_usd[temp].metrics.market_data.price_usd
+      let temp_name=price_usd[temp].slug
+      top_gainer={id:i,name:temp_name,value:price}
+      Highlight.value.push(top_gainer)
+    }
+  })
+
+</script>
+
+<template>
+<section class="main-container-market">
+  <div class="top-market">
+    <div class="market-1 box">
+      <h2 class="market-1-txt placeholder-txt">Top Gainer</h2>
+      <ul class="names-container-1">
+        <li class="list-name-1" v-for="i in top" :key="i.id">
+          {{i.name}} <div class="list-coin-name">{{i.value}}</div>
+        </li>
+      </ul>
+   </div>
+
+    <div class="market-2 box">
+      <h2 class="market-2-txt placeholder-txt">Larg Volume</h2>
+      <ul class="names-container-1">
+        <li class="list-name-1">BTC <div class="list-coin-name"> {{large_volume[0]}}</div>   </li>
+        <li class="list-name-1">ETH  <div class="list-coin-name">{{large_volume[1]}}</div></li>
+        <li class="list-name-1">USDT<div class="list-coin-name">{{large_volume[2]}}</div></li>
+      </ul>
+    </div>
+
+    <div class="market-3 box">
+     <h2 class="market-3-txt placeholder-txt">Highlight coin</h2> 
+     <div class="names-container-1">
+      <li class="list-name-1" v-for="i in Highlight" :key="i.id">
+        {{i.name}} <div class="list-coin-name">{{i.value}}</div>
+      </li>
+     </div>
+    </div>
+ </div>
+</section>
+</template>
+
+<style scoped>
+:root{
+  --allview:100vw;
+  --topmargin:50px;
+}
+*{
+  margin: 0;
+  padding: 0;
+}
+.main-container-market{
+  width: var(--allview);
+  height: 100vh;
+}
+.top-market{
+  width: var(--allview);
+  height: 335px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+}
+.market-1,.market-2,.market-3{
+  width: 370px;
+  height: 270px;
+  margin-top: var(--topmargin);
+  margin-top: 60px;
+}
+
+.market-2{
+ margin-left: 40px;
+ margin-right: 40px;
+}
+
+.placeholder-txt{
+  font-size: large;
+}
+
+.names-container-1{
+  list-style-type: none;
+  margin-top: 30px;
+  margin-left: 7px;
+  font-size: 1.3rem;
+}
+.list-name-1{
+  padding: 10px;
+  display:  flex;
+  flex-direction: row;
+  align-items: flex-start;
+}
+.list-coin-name{
+  padding-left: 37px;
+}
+</style>
+
+
--- frontend/src/views/Account/Market.vue
+++ frontend/src/views/Account/Market.vue
@@ -1,94 +1,9 @@
 <template>
-<section class="main-container-market">
-  <div class="top-market">
-    <div class="market-1">
-      <h2 class="market-1-txt placeholder-txt">Top Gainer</h2>
-   </div>
-
-    <div class="market-2">
-      <h2 class="market-2-txt placeholder-txt">Larg Volume</h2>
-      <ul class="names-container-1">
-        <li class="list-name-1">BTC <div class="list-coin-name">1900</div>   </li>
-        <li class="list-name-1">ETH  <div class="list-coin-name">{{price_usd}}</div></li>
-        <li class="list-name-1">BNB <div class="list-coin-name">500</div></li>
-      </ul>
-    </div>
-
-    <div class="market-3">
-     <h2 class="market-3-txt placeholder-txt">Highlight coin</h2> 
-    </div>
- </div>
-</section>
-
+  <div>
+    <MarketTop/>
+  </div>
 </template>
 
-<style scoped>
-:root{
-  --allview:100vw;
-  --topmargin:50px;
-}
-*{
-  margin: 0;
-  padding: 0;
-}
-.main-container-market{
-  width: var(--allview);
-  height: 100vh;
-}
-.top-market{
-  width: var(--allview);
-  height: 335px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: flex-start;
-}
-.market-1,.market-2,.market-3{
-  width: 300px;
-  height: 215px;
-  margin-top: var(--topmargin);
-  margin: 21px;
-  margin-top: 60px;
-}
-.market-1{
- background-color: blue;
-}
-.market-2{
- background-color: yellow;
-}
-.market-3{
- background-color: brown;
-}
-.placeholder-txt{
-  font-size: large;
-}
-
-.names-container-1{
-  list-style-type: none;
-  margin-top: 30px;
-  margin-left: 7px;
-  font-size: 1.3rem;
-}
-.list-name-1{
-  padding: 10px;
-  display:  flex;
-  flex-direction: row;
-  align-items: flex-start;
-}
-.list-coin-name{
-  padding-left: 37px;
-}
-</style>
-
 <script setup>
-import {ref} from 'vue'
-
-let price_usd=ref(0)
-  fetch('https://data.messari.io/api/v1/assets/eth/metrics')
- .then(res=>res.json())
-  .then(data=>{
-    price_usd.value=data.data.market_data.price_usd
-  })
-
+import MarketTop from '@/components/MarketTop.vue'
 </script>
-
