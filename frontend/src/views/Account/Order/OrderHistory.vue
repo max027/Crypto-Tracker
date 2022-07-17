d959a943ba35ea23d2641c9b2339e6a5622aae7e
max <max@gmail.com>
2022-07-17 17:59:56 +0530

    updated order history

--- frontend/src/views/Account/Order/OrderHistory.vue
+++ frontend/src/views/Account/Order/OrderHistory.vue
@@ -2,40 +2,75 @@
     <div >
         <div class="main-cont-order">
         <div class="title-container-order">
-            <div class="title-name-order">
-                Name
-            </div>
-            <div class="title-ordertype-order">
-                Order Type
-            </div>
-            <div class="title-price-order">
-                Price
-            </div>
-            <div class="title-date-order">
-                Date
-            </div>
-            <div class="title-qty-order">
-                qty
-            </div>
+        <div class="title-cointainer-bottom">
+            <h3 class="title-name">Name</h3> 
+            <div class="title-qty">Qty</div>
+            <h3 class="title-price">Price</h3>
+            <h3 class="title-change">Order-Type</h3>
+        </div> 
+        <div class="seperator"></div>
+                <div class="main-content-data">
+            <ul>
+                <li class="box order_history content-wrapper" v-for="i in order_history" :key="i.id">
+                    <div class="coin-id">{{i.id}}</div>
+                    <div class="coin-name">{{i.coin_name}}</div> 
+                    <div class="coin-qty">{{i.order_qty}}</div>
+                    <div class="coin-price">{{i.order_price}}</div>
+                    <div class="order-type">{{i.order_type}}</div>
+                </li>
+            </ul>
         </div>
-
-        <h1>price:{{order_history}}</h1>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import {ref} from 'vue'
+import axios from 'axios'
 let order_history=ref([])
-fetch('http://127.0.0.1:8000/api/v1/orderhistory').then(res=>res.json()).then(data=>{
-    
+
+axios.get('/api/v1/getorder/')
+.then(res=>{
+    order_history.value=res.data
 })
 </script>
 
-<style>
-.main-cont-order{
-    width: 100vw;
-    height: 70px;
-    background-color: #cccccc;
+<style scoped>
+.order_history{
+    display: flex;
+}
+.content-wrapper :nth-child(1){
+    width: 20px;
+}
+.content-wrapper :nth-child(2){
+  width: 100px;
+}
+.content-wrapper :nth-child(3){
+  width: 100px;
+}
+.content-wrapper :nth-child(4){
+  width: 500px;
+}
+.title-cointainer-bottom{
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  font-size: large;
+}
+.title-name{
+  margin-left: 3px;
+}
+.title-price{
+  margin-left: 13%;
+}
+.title-change{
+  margin-left: 20%;
+}
+.title-qty{
+    margin-left: 5%;
+}
+.seperator{
+    border: 1px solid black;
 }
 </style>
